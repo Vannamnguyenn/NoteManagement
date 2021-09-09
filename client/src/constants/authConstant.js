84c8dd19382dc6f4bf778fed42bf9c1c7f98910d
Nguyen Ngoc Khanh <vannamnguyen928@gmail.com>
2021-09-09 15:13:23 +0700

    test

--- client/src/constants/authConstant.js
+++ client/src/constants/authConstant.js
@@ -1,5 +1,5 @@
 export const AUTH_API =
   process.env.NODE_ENV !== "production"
     ? "https://glacial-river-91159.herokuapp.com/api"
-    : "urlDeploy";
+    : "https://glacial-river-91159.herokuapp.com/api";
 export const LOCAL_STORAGE_TOKEN_NAME = "learn-mern";
