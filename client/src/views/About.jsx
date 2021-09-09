fb5db2ce66a8e7163cebca396879df0f747f6284
Nguyen Ngoc Khanh <vannamnguyen928@gmail.com>
2021-09-09 15:30:24 +0700

    a

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -4,7 +4,11 @@ import Button from "react-bootstrap/Button";
 const About = () => {
   return (
     <div className="justify-content-center d-flex mt-5">
-      <Button variant="primary" size="lg" href="https://www.github.com">
+      <Button
+        variant="primary"
+        size="lg"
+        href="https://github.com/Vannamnguyenn/NoteManagement"
+      >
         Download source code in github.com
       </Button>
     </div>
